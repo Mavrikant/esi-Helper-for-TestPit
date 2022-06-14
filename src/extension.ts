7155c570c80f17ab060b7c3ac951320b0be65a13
M. Serdar Karaman <m.serdar.karaman@gmail.com>
2022-06-14 15:50:38 +0300

    first package 0.0.1

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+	console.log('Congratulations, your extension "ESI" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World! from ESEN');
+		vscode.window.showInformationMessage('Hello from ESI');
 	});
 
 	context.subscriptions.push(disposable);
