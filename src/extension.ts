395b688ef9afd3a0e2ae1367520ad69b41faaa10
M. Serdar Karaman <m.serdar.karaman@gmail.com>
2022-08-18 17:34:52 +0300

    v0.0.12 - openwith command is added

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,29 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from "child_process";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "esi Helper for TestPit" is now active!');
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "esi Helper for TestPit" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from esi Helper!');
-	});
+    const disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+        vscode.window.showInformationMessage('Hello World from esi Helper!');
+    });
 
-	context.subscriptions.push(disposable);
+    const disposable2 = vscode.commands.registerCommand('extension.openWithTestPit', async () => {
+        const execSync = cp.exec;
+        const currentlyOpenTabfilePath = vscode.window.activeTextEditor?.document.uri.fsPath;
+        execSync("\"C:\\Program Files (x86)\\TestPit\\Tools\\bin\\TestPit.exe\" --sf="+ currentlyOpenTabfilePath );
+    });
+
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable2);
 }
