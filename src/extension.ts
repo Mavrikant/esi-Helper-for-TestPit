f7765f860193b87a39e56d194e93e3dfa51f16fa
M. Serdar Karaman <m.serdar.karaman@gmail.com>
2022-12-30 17:50:44 +0300

    v0.0.17 update step numbers

--- src/extension.ts
+++ src/extension.ts
@@ -24,6 +24,43 @@ export function activate(context: vscode.ExtensionContext) {
         execSync("\"C:\\Program Files (x86)\\TestPit\\Tools\\bin\\TestPit.exe\" --sf="+ currentlyOpenTabfilePath );
     });
 
+    const disposable3 = vscode.commands.registerCommand('extension.updateStepNumbers', async () => {
+        // Get the active text editor
+        const editor = vscode.window.activeTextEditor;
+
+        if (editor) {
+            const fullText = editor.document.getText();
+            const firstLine = editor.document.lineAt(0);
+            const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+            const textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+            let number = 0;
+            let targetText = fullText.replaceAll(/\[STEP \d+\]/g,"[STEP XX]");
+            const count = (fullText.match(/\[STEP \d+\]/g) || []).length;
+            for (let i = 0; i < count; i++) {
+                targetText = targetText.replace("[STEP XX]",function() {
+                    // will be called if a "A" gets found
+                    number=number+10;        // increases count of "number"
+                    const result = "[STEP " + number.toString() + "]";
+                    return result;      // return will replace "A" with "1"
+                });
+            }
+            number = 0;
+            targetText= targetText.replaceAll(/\[\/STEP \d+\]/g,"[/STEP XX]");
+            for (let i = 0; i < count; i++) {
+                targetText = targetText.replace("[/STEP XX]",function() {
+                    // will be called if a "A" gets found
+                    number=number+10;        // increases count of "number"
+                    const result = "[/STEP " + number.toString() + "]";
+                    return result;      // return will replace "A" with "1"
+                });
+            }
+            editor.edit(editBuilder => {
+				editBuilder.replace(textRange, targetText);
+            });
+        }
+    });
+
     context.subscriptions.push(disposable);
     context.subscriptions.push(disposable2);
+    context.subscriptions.push(disposable3);
 }
