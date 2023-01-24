17cb198e4f5aa03884db466c30f41a42c26149bd
M. Serdar Karaman <m.serdar.karaman@gmail.com>
2023-01-24 10:45:24 +0300

    v0.0.24 +refactoring command

--- src/extension.ts
+++ src/extension.ts
@@ -1,99 +1,157 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import * as cp from "child_process";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "esi Helper for TestPit" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "esi Helper for TestPit" is now active!'
+  );
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
 
-    const disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        vscode.window.showInformationMessage('Hello World from esi Helper!');
-    });
+  const disposable = vscode.commands.registerCommand(
+    "extension.sayHello",
+    () => {
+      vscode.window.showInformationMessage("Hello World from esi Helper!");
+    }
+  );
 
-    const disposable2 = vscode.commands.registerCommand('extension.openWithTestPit', async () => {
-        const execSync = cp.exec;
-        const currentlyOpenTabfilePath = vscode.window.activeTextEditor?.document.uri.fsPath;
-        execSync("\"C:\\Program Files (x86)\\TestPit\\Tools\\bin\\TestPit.exe\" --sf="+ currentlyOpenTabfilePath );
-    });
-
-    const disposable3 = vscode.commands.registerCommand('extension.updateStepNumbers', updateStepNumbers);
-
-    async function updateStepNumbers() {
-        // Get the active text editor
-        const editor = vscode.window.activeTextEditor;
-      
-        if (!editor) {
-          return;
-        }
-      
-        const fullText = editor.document.getText();
-        const firstLine = editor.document.lineAt(0);
-        const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-        const textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-      
-        let targetText = fullText.replace(/\[STEP \d+\]/g, '[STEP XX]');
-        const stepCount = (fullText.match(/\[STEP \d+\]/g) || []).length;
-      
-        for (let i = 0; i < stepCount; i++) {
-          targetText = targetText.replace('[STEP XX]', function() {
-            const number = (i + 1) * 10;
-            return `[STEP ${number}]`;
-          });
-        }
-      
-        targetText = targetText.replace(/\[\/STEP \d+\]/g, '[/STEP XX]');
-      
-        for (let i = 0; i < stepCount; i++) {
-          targetText = targetText.replace('[/STEP XX]', function() {
-            const number = (i + 1) * 10;
-            return `[/STEP ${number}]`;
-          });
-        }
-      
-        editor.edit((editBuilder) => {
-          editBuilder.replace(textRange, targetText);
-        });
-      }
-
-      const disposable4 = vscode.commands.registerCommand(
-        "extension.gotoStep",
-        async () => {
-          const editor = vscode.window.activeTextEditor;
-          if (!editor) {
-            return vscode.window.showInformationMessage(
-              "No active editor found."
-            );
-          }
-          const searchQuery = await vscode.window.showInputBox({
-            placeHolder: "Step number",
-            prompt: "Enter the step number you want to go to",
-          });
-          if (!searchQuery) {
-            return;
-          }
-          const stepNumberStr = String(searchQuery);
-          const stepRegex = new RegExp(`\\[STEP ${stepNumberStr}\\]`);
-          const lines = editor.document.getText().split("\n");
-          const lineNumber = lines.findIndex((line) => stepRegex.test(line));
-          if (lineNumber === -1) {
-            return vscode.window.showInformationMessage("😔 Step \"" + stepNumberStr+ "\" not found!");
-          }
-          const range = editor.document.lineAt(lineNumber).range;
-          editor.selection = new vscode.Selection(range.start, range.end);
-          editor.revealRange(range, vscode.TextEditorRevealType.AtTop);
-        }
+  const disposable2 = vscode.commands.registerCommand(
+    "extension.openWithTestPit",
+    async () => {
+      const execSync = cp.exec;
+      const currentlyOpenTabfilePath =
+        vscode.window.activeTextEditor?.document.uri.fsPath;
+      execSync(
+        '"C:\\Program Files (x86)\\TestPit\\Tools\\bin\\TestPit.exe" --sf=' +
+          currentlyOpenTabfilePath
       );
+    }
+  );
 
-    context.subscriptions.push(disposable);
-    context.subscriptions.push(disposable2);
-    context.subscriptions.push(disposable3);
-    context.subscriptions.push(disposable4);
+  const disposable3 = vscode.commands.registerCommand(
+    "extension.updateStepNumbers",
+    updateStepNumbers
+  );
+
+  async function updateStepNumbers() {
+    // Get the active text editor
+    const editor = vscode.window.activeTextEditor;
+
+    if (!editor) {
+      return;
+    }
+
+    const fullText = editor.document.getText();
+    const firstLine = editor.document.lineAt(0);
+    const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+    const textRange = new vscode.Range(
+      firstLine.range.start,
+      lastLine.range.end
+    );
+
+    let targetText = fullText.replace(/\[STEP \d+\]/g, "[STEP XX]");
+    const stepCount = (fullText.match(/\[STEP \d+\]/g) || []).length;
+
+    for (let i = 0; i < stepCount; i++) {
+      targetText = targetText.replace("[STEP XX]", function () {
+        const number = (i + 1) * 10;
+        return `[STEP ${number}]`;
+      });
+    }
+
+    targetText = targetText.replace(/\[\/STEP \d+\]/g, "[/STEP XX]");
+
+    for (let i = 0; i < stepCount; i++) {
+      targetText = targetText.replace("[/STEP XX]", function () {
+        const number = (i + 1) * 10;
+        return `[/STEP ${number}]`;
+      });
+    }
+
+    editor.edit((editBuilder) => {
+      editBuilder.replace(textRange, targetText);
+    });
+  }
+
+  const disposable4 = vscode.commands.registerCommand(
+    "extension.gotoStep",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+      const searchQuery = await vscode.window.showInputBox({
+        placeHolder: "Step number",
+        prompt: "Enter the step number you want to go to",
+      });
+      if (!searchQuery) {
+        return;
+      }
+      const stepNumberStr = String(searchQuery);
+      const stepRegex = new RegExp(`\\[STEP ${stepNumberStr}\\]`);
+      const lines = editor.document.getText().split("\n");
+      const lineNumber = lines.findIndex((line) => stepRegex.test(line));
+      if (lineNumber === -1) {
+        return vscode.window.showInformationMessage(
+          '😔 Step "' + stepNumberStr + '" not found!'
+        );
+      }
+      const range = editor.document.lineAt(lineNumber).range;
+      editor.selection = new vscode.Selection(range.start, range.end);
+      editor.revealRange(range, vscode.TextEditorRevealType.AtTop);
+    }
+  );
+
+  const disposable5 = vscode.commands.registerCommand(
+    "extension.refactorDocument",
+    refactorDocument
+  );
+
+  async function refactorDocument() {
+    // Get the current editor
+    const editor = vscode.window.activeTextEditor;
+
+    // If there's no open editor, do nothing
+    if (!editor) {
+      return;
+    }
+
+    // Get the entire text of the document
+    const text = editor.document.getText();
+
+    // Split the text into an array of lines
+    const lines = text.split("\n");
+
+    // Trim trailing spaces from each line
+    const trimmedLines = lines.map((line) => line.trimEnd());
+
+    // Join the lines back into a single string
+    const trimmedText = trimmedLines.join("\n");
+
+    // Replace tabs with 4 spaces and trim trailing spaces
+    const replacedText = trimmedText.replace(/\t/g, "    ");
+
+    // Replace the entire text of the document with the trimmed text
+    editor.edit((editBuilder) => {
+      const fullDocRange = new vscode.Range(
+        editor.document.positionAt(0),
+        editor.document.positionAt(text.length)
+      );
+      editBuilder.replace(fullDocRange, replacedText);
+    });
+  }
+
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable2);
+  context.subscriptions.push(disposable3);
+  context.subscriptions.push(disposable4);
+  context.subscriptions.push(disposable5);
 }
